21503f4a01d19db4eb557492c4e5c6a7f34a7e1b
Jack Ponder <jackponder3@gmail.com>
2023-06-28 16:39:17 -0400

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# App extensions
+db = SQLAlchemy()
+
+
+def create_app():
+    # Configure app
+    app = Flask(__name__)
+
+    # Configure database
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+    db.init_app(app)
+
+    # Configure routes
+    from app.routes import routes
+    app.register_blueprint(routes)
+
+    # Configure models
+    with app.app_context():
+        db.create_all()
+
+    return app
