6045629df7df0d8ae7806aff9b6e607020534cb8
Jack Ponder <jackponder3@gmail.com>
2023-06-29 13:26:09 -0400

    added poll routes and models

--- app/models.py
+++ app/models.py
@@ -1,6 +1,12 @@
-from datetime import date
+from datetime import date, datetime
 
 from . import db
+
+voters_table = db.Table(
+    "voters",
+    db.Column("user_id", db.Integer, db.ForeignKey("users.id")),
+    db.Column("poll_id", db.Integer, db.ForeignKey("polls.id")),
+)
 
 
 class User(db.Model):
@@ -10,6 +16,7 @@ class User(db.Model):
     username = db.Column(db.Text, unique=True, nullable=False)
     password = db.Column(db.Text, nullable=False)
     date_joined = db.Column(db.Date, nullable=False, default=date.today())
+    polls = db.relationship("Poll", backref="creator")
 
     def __repr__(self):
         return f"User #{self.id}"
@@ -19,4 +26,48 @@ class User(db.Model):
             "id": self.id,
             "username": self.username,
             "dateJoined": self.date_joined,
+        }
+
+
+class Poll(db.Model):
+    __tablename__ = "polls"
+
+    id = db.Column(db.Integer, primary_key=True)
+    creator_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    title = db.Column(db.Text, nullable=False)
+    options = db.relationship("PollOption", backref="poll")
+    tag = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.today())
+    voters = db.relationship("User", secondary=voters_table, backref="voted_polls")
+
+    def __repr__(self):
+        return f"Poll #{self.id}"
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "creatorId": self.creator_id,
+            "title": self.title,
+            "options": [option.serialize() for option in self.options],
+            "tag": self.tag,
+            "timestamp": self.timestamp,
+            "voters": [voter.id for voter in self.voters]
+        }
+
+
+class PollOption(db.Model):
+    __tablename__ = "poll_options"
+
+    id = db.Column(db.Integer, primary_key=True)
+    poll_id = db.Column(db.Integer, db.ForeignKey("polls.id"), nullable=False)
+    name = db.Column(db.Text, nullable=False)
+    votes = db.Column(db.Integer, nullable=False, default=0)
+
+    def __repr__(self):
+        return f"Poll Option #{self.id}"
+    
+    def serialize(self):
+        return {
+            "name": self.name,
+            "votes": self.votes
         }
--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify, url_for, request
 from werkzeug.security import generate_password_hash
 
 from . import db
-from .models import User
+from .models import User, Poll, PollOption
 from .errors import error
 
 routes = Blueprint("routes", __name__)
@@ -10,7 +10,7 @@ routes = Blueprint("routes", __name__)
 
 @routes.route("/users/<int:id>", methods=["GET"])
 def get_user(id: int):
-    """Get a user by their id from the database"""
+    """Get a user by their id"""
 
     # Query user from database
     user = db.session.get(User, id)
@@ -24,12 +24,12 @@ def get_user(id: int):
 
 @routes.route("/users", methods=["POST"])
 def create_user():
-    """Add a new user to the database"""
+    """Create a new user"""
 
     # Ensure correct data was submitted
-    data = request.get_json() or {}
-    username = data.get("username")
-    password = data.get("password")
+    json = request.get_json() or {}
+    username = json.get("username")
+    password = json.get("password")
     if not username or not password:
         return error(message="invalid data")
     
@@ -46,4 +46,50 @@ def create_user():
     response = jsonify(new_user.serialize())
     response.status_code = 201
     response.headers["Location"] = url_for("routes.get_user", id=new_user.id)
+    return response
+
+
+@routes.route("/polls/<int:id>", methods=["GET"])
+def get_poll(id: int):
+    """Get a poll by its id"""
+    
+    # Query database for poll
+    poll = db.session.get(Poll, id)
+
+    # Return an error if no user was found
+    if not poll:
+        return error(404, "invalid poll id")
+    
+    return jsonify(poll.serialize())
+
+
+@routes.route("/polls", methods=["POST"])
+def create_poll():
+    """Create a new poll"""
+
+    # Ensure correct data was submitted
+    json = request.get_json() or {}
+    creator = db.session.get(User, json.get("creatorId"))
+    title = json.get("title")
+    options = json.get("options")
+    tag = json.get("tag")
+    if not creator or not title or type(options) != list or len(options) < 2:
+        return error(message="invalid form data")
+
+    # Add new poll to database
+    new_poll = Poll(creator=creator, title=title)
+    if tag:
+        new_poll.tag = tag
+    db.session.add(new_poll)
+    for option in options:
+        if not option:
+            continue
+        new_option = PollOption(poll=new_poll, name=option)
+        db.session.add(new_option)
+    db.session.commit()
+
+    # Return newly created poll
+    response = jsonify(new_poll.serialize())
+    response.status_code = 201
+    response.headers["Location"] = url_for("routes.get_poll", id=new_poll.id)
     return response
