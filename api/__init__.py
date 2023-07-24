d97fabe691d3b6050bbc851dc759f9e228b7941b
Jack Ponder <jackponder3@gmail.com>
2023-07-24 02:35:52 -0400

    updated app config with environment variables

--- api/__init__.py
+++ api/__init__.py
@@ -2,6 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 # App extensions
 db = SQLAlchemy()
 cors = CORS()
@@ -10,12 +15,12 @@ cors = CORS()
 def create_app():
     # Configure app
     app = Flask(__name__)
+    app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     # Configure database
     from . import models
-    app.config["SECRET_KEY"] = "secret_key"
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     with app.app_context():
         db.create_all()
