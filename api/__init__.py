1dc5c2bfe80e99cff0f0274e364cbaf1426df0ce
Jack Ponder <jackponder3@gmail.com>
2023-07-28 01:59:33 -0400

    stopped sorting of json keys to fix error when sorting numeric and string keys during validation errors

--- api/__init__.py
+++ api/__init__.py
@@ -12,6 +12,7 @@ cors = CORS()
 def create_app():
     # Configure app
     app = Flask(__name__)
+    app.json.sort_keys = False
     app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI").replace("postgres://", "postgresql://")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
