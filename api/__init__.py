21c9f415d6200a26166eef17733ef718e73d8b67
Jack Ponder <jackponder3@gmail.com>
2023-07-29 02:34:40 -0400

    added pool pre ping to db engine options

--- api/__init__.py
+++ api/__init__.py
@@ -16,6 +16,7 @@ def create_app():
     app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI").replace("postgres://", "postgresql://")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {"pool_pre_ping": True}
 
     # Configure database
     from . import models
