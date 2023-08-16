96d2369c4d686573a06d0ae19c2727697d4f2729
Jack <jackponder3@gmail.com>
2023-08-16 12:45:04 -0400

    fixed vercel deployment

--- api/db/session.py
+++ api/db/session.py
@@ -3,5 +3,5 @@ from sqlalchemy.orm import sessionmaker
 
 import os
 
-engine = create_engine(os.environ.get("DATABASE_URI"))
+engine = create_engine(os.environ.get("DATABASE_URI").replace("postgres://", "postgresql://"))
 SessionLocal = sessionmaker(engine)
